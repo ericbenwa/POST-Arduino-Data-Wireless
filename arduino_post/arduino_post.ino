e9bf0ea865542ded83ccea791b80a8c8e4ade166
Eric Benoit <eric@goinvo.com>
2013-05-05 17:54:57 -0400

    Renamed and moved methods

--- arduino_post/arduino_post.ino
+++ arduino_post/arduino_post.ino
@@ -1,37 +1,26 @@
 #include <SPI.h>
 #include <WiFi.h>
 
-//
-// EDIT: change the SSID and password to match your network
-//
-char ssid[] = "yournetwork";  // your network SSID (name)
-char pass[] = "yourpassword";        // your network password
+// EDIT: Change the 'ssid' and 'password' to match your network
+char ssid[] = "flux capacitor";  // your network SSID (name)
+char pass[] = "applepie"; // your network password
 int status = WL_IDLE_STATUS;
 WiFiClient client;
 
-//
-// EDIT: server address to match your domain
-//
-char server[] = "www.yourdomain.com";
+// EDIT: 'Server' address to match your domain
+char server[] = "www.ebenoit.com";
 
-// this is the data that will be passed into your POST and matches your mysql column
+// This is the data that will be passed into your POST and matches your mysql column
 int yourarduinodata = 999;
 String yourdatacolumn = "yourdata=";
 String yourdata;
 
 void setup() {
   Serial.begin(9600);
+  
+  connectWifi();
 
-  // attempt to connect to wifi network
-  while ( status != WL_CONNECTED) {
-    Serial.print("Attempting to connect to SSID: ");
-    Serial.println(ssid);
-    status = WiFi.begin(ssid, pass);
-    // wait 10 seconds for connection
-    delay(10000);
-  }
-
-  // you're connected now, so print out the status
+  // You're connected now, so print out the status
   printWifiStatus();
   
   // Combine yourdatacolumn header (yourdata=) with the data recorded from your arduino
@@ -39,55 +28,62 @@ void setup() {
   // sent in your POST request
   yourdata = yourdatacolumn + yourarduinodata;
   
-  httpRequest();
+  postData();
 }
 
 void loop() {
 
 }
 
+void connectWifi() {
+  // Attempt to connect to wifi network
+  while ( status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    status = WiFi.begin(ssid, pass);
+    // Wait 10 seconds for connection
+    delay(10000);
+  }
+}
+
 void printWifiStatus() {
-  // print the SSID of the network you're attached to
+  // Print the SSID of the network you're attached to
   Serial.print("SSID: ");
   Serial.println(WiFi.SSID());
 
-  // print your WiFi shield's IP address
+  // Print your WiFi shield's IP address
   IPAddress ip = WiFi.localIP();
   Serial.print("IP Address: ");
   Serial.println(ip);
 
-  // print the received signal strength
+  // Print the received signal strength
   long rssi = WiFi.RSSI();
   Serial.print("signal strength (RSSI):");
   Serial.print(rssi);
   Serial.println(" dBm");
 }
 
-// this method makes a HTTP connection to the server
-void httpRequest() {
-  // if there's a successful connection, send the HTTP PUT request
+// This method makes a HTTP connection to the server and POSTs data
+void postData() {
+  // If there's a successful connection, send the HTTP POST request
   if (client.connect(server, 80)) {
     Serial.println("connecting...");
 
-    //
-    // EDIT: the URL to the location of your insert_mysql.php on your web-host
-    //
-    client.println("POST /insert_mysql.php HTTP/1.1");
+    // EDIT: The POST 'URL' to the location of your insert_mysql.php on your web-host
+    client.println("POST /client/EJB/testdatabase/insert_mysql.php HTTP/1.1");
 
-    //
-    // EDIT: Host to match your domain
-    //
-    client.println("Host: www.yourdomain.com");
+    // EDIT: 'Host' to match your domain
+    client.println("Host: www.ebenoit.com");
     client.println("User-Agent: Arduino/1.0");
     client.println("Connection: close");
     client.println("Content-Type: application/x-www-form-urlencoded;");
     client.print("Content-Length: ");
     client.println(yourdata.length());
     client.println();
-    client.println(yourdata);
+    client.println(yourdata); 
   } 
   else {
-    // if you couldn't make a connection:
+    // If you couldn't make a connection:
     Serial.println("Connection failed");
     Serial.println("Disconnecting.");
     client.stop();
